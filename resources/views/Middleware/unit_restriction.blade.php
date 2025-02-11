cce02074f572ec082e19a5ecce2dac34e7af8a58
jordhi404 <tejosaputro22@gmail.com>
2025-02-11 11:50:41 +0700

    update untuk laman ip dibatasi

--- resources/views/Middleware/unit_restriction.blade.php
+++ resources/views/Middleware/unit_restriction.blade.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UNIT ACCESS DENIED</title>
+    <style>
+        body {
+            padding: 0;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin-top: 20%;
+        }
+    </style>
 </head>
 <body>
     <h1>UNIT ACCES RESTRICTED</h1>
-    <p>Laman ini dibatasi untuk unit ini.</p>
+    <p>Unit ini tidak diberi akses ke laman ini.</p>
 </body>
 </html>
